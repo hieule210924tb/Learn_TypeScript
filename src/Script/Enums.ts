4250ed39460d837273202c3947c1720ab5f146e4
hieule210924tb <manu210924@gmail.com>
2025-11-26 08:46:21 +0700

    Enums type

---
+++ src/Script/Enums.ts
@@ -0,0 +1,16 @@
+//Number enums
+enum Direction {
+  top = 1,
+  right,
+  bottom,
+  left,
+}
+
+//String enums
+enum Tag {
+  POPULAR = "POPULAR",
+  RECENT = "RECENT",
+  LATEST = "LATEST",
+  NEW = "NEW",
+}
+console.log(Direction.bottom, Tag.POPULAR);
